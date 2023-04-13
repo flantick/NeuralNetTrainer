973b5a537785c31b0abb965f86ff54ab25ecf181
flantick <flantick@gmail.com>
2023-04-13 22:45:47 +0300

    a bit of cleaning

--- Segmenter.py
+++ Segmenter.py
@@ -1,7 +1,7 @@
 import torch
 from torch.optim import Adam
 from torchmetrics.functional import accuracy
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 
 
@@ -54,8 +54,8 @@ class Segmenter (pl.LightningModule):
 
 
 
-        self.vl_loss = []
-        self.vl_ac = []
+        self.vl_loss = torch.Tensor([])
+        self.vl_acc = torch.Tensor([])
 
     def forward(self, input_ids):
         x = self.model(input_ids)
@@ -72,19 +72,20 @@ class Segmenter (pl.LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
-        input_ids = batch[0]
-        labels = batch[1]
+        input_ids, labels = batch
+        with torch.no_grad():
+            logits = self(input_ids)
+            loss = self.func_loss(logits, labels)
 
-        logits = self(input_ids)
-        loss = self.func_loss(logits, labels)
-
-        acc = accuracy(logits.squeeze() ,labels.squeeze().to(torch.int), task=self.task, num_labels=self.num_labels,
-                       num_classes=self.num_classes) #<--check this
+        acc = accuracy(logits.squeeze(), labels.squeeze().to(torch.int), task=self.task, num_labels=self.num_labels,
+                       num_classes=self.num_classes)
 
         metrics = {"val_loss": loss, "val_acc": acc}
-        self.log_dict(metrics)
-        self.vl_loss.append(loss)
-        self.vl_ac.append(acc)
+        self.log("val_loss", loss)
+        self.log("val_acc", acc)
+
+        self.vl_loss = torch.cat([self.vl_loss, loss.unsqueeze(0).to('cpu')])
+        self.vl_acc = torch.cat([self.vl_acc, acc.unsqueeze(0).to('cpu')])
 
         return metrics
 
@@ -95,71 +96,44 @@ class Segmenter (pl.LightningModule):
 
         return preds
 
-    # def prepare_data(self):
-    #     global data_t, data_v
-    #     data = pd.read_csv('dataset/train/segmentation.csv')
-    #     data_t, data_v = train_test_split(data, train_size=0.8)
-    #
-    # def prepare_for_pred(self, CSVPath):
-    #     global pred_data
-    #     pred_data = pd.read_csv(CSVPath)
-
     def train_dataloader(self):
-
-        # train_dataset = UrDataset(data_t)  # problem mb here
-        # Data loader
         train_dataloader = DataLoader(
-            self.train_torch_dataset,  # The training samples.
-            # sampler = RandomSampler(train_dataset), # Select batches randomly
-            batch_size = self.batch_size # Trains with this batch size.
+            self.train_torch_dataset,
+            batch_size=self.batch_size
         )
         return train_dataloader
 
     def val_dataloader(self):
-
-        # val_dataset = UrDataset(data_v)
-        # Data loader
         validation_dataloader = DataLoader(
-            self.val_torch_dataset, # The validation samples.
-            # sampler = SequentialSampler(val_dataset), # Pull out batches sequentially.
-            batch_size=self.batch_size # Evaluate with this batch size.
+            self.val_torch_dataset,
+            batch_size=self.batch_size
         )
         return validation_dataloader
 
     def predict_dataloader(self):
         if self.pred_torch_dataset is None:
             raise ValueError("pred_torch_dataset is None")
-        # pred_dataset = UrDataset(pred_data)
-        # Data loader
         pred_dataloader = DataLoader(
-            self.pred_torch_dataset,  # The validation samples.
-            # sampler = SequentialSampler(val_dataset), # Pull out batches sequentially.
-            batch_size=self.batch_size  # Evaluate with this batch size.
+            self.pred_torch_dataset,
+            batch_size=self.batch_size
         )
         return pred_dataloader
 
     def on_validation_epoch_end(self):
-        # outputs = list of dictionaries
-        # avg = outputs[0]
-        # for x in outputs:
-        #  avg = torch.cat((avg,x), 0)
+        val_loss_mean = torch.mean(self.vl_loss)
+        val_acc_mean = torch.mean(self.vl_acc)
 
-        # avg_loss = avg.mean()
-        avg_loss = torch.stack([x for x in self.vl_loss]).mean()
-        avg_acc = torch.stack([x for x in self.vl_ac]).mean()
-        # tensorboard_logs = {'avg_val_loss': avg_loss}
-        # use key 'log'
-        print('\n=======\n' + 'Mean validation loss ' + avg_loss.__str__()[7:13])
-        print('\nMean validation accuracy '+avg_acc.__str__()[7:13]+'\n=======\n')
-        self.vl_loss = []
-        self.vl_ac = []
-        return {'val_loss': avg_loss, "val_acc": avg_acc}  # , 'log': tensorboard_logs}
+        print(f"\n=======\nMean validation loss: {round(val_loss_mean.item(), 6)}")
+        print(f"Mean validation accuracy: {round(val_acc_mean.item(), 6)}\n=======\n")
+
+        self.vl_loss = torch.Tensor([])
+        self.vl_acc = torch.Tensor([])
+
+        return {'val_loss': val_loss_mean, "val_acc": val_acc_mean}
 
     def configure_optimizers(self):
         optimizer = Adam(self.parameters(),
-                         lr=self.learning_rate,  # args.learning_rate - default is 5e-5, our notebook had 2e-5 (1e-3)
+                         lr=self.learning_rate,
                          )
 
         return {"optimizer": optimizer}
-
-
